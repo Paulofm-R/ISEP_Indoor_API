48807edbc9da5cdfa45c54a48eeb9daa5b53f91c
PaulofmRodrigues <paulofilipe_100pt@hotmail.com>
2024-04-18 18:44:40 +0100

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -26,17 +26,25 @@ router.post('/register', [
 })
 
 router.get('/', async (req, res) => {
-    await utilities.isAdmin(req, res);
+    // await utilities.isAdmin(req, res);
     userController.getAll(req, res);
 })
 
-router.get('/:userID', utilities.validateToken, (req, res) => {
+/*router.get('/:userID', utilities.validateToken, (req, res) => {
+    userController.findUser(req, res);
+})*/
+
+router.get('/:userID', (req, res) => {
     userController.findUser(req, res);
 })
 
-router.put('/:userID', utilities.validateToken, (req, res) => {
+/* router.put('/:userID', utilities.validateToken, (req, res) => {
     userController.update(req, res);
-})
+}) */
+
+router.put('/:userID', (req, res) => {
+    userController.update(req, res);
+}) 
 
 router.delete('/:userID', utilities.validateToken, (req, res) => {
     userController.delete(req, res);
