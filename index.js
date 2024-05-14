2421b12d0d17056c5c6f015b314dc0c04e26bc9a
André Oliveira Gonçalves <1191660@isep.ipp.pt>
2024-05-14 11:18:08 +0100

    added_Cors

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-require('dotenv').config(); // read environment variables from .env file
-const express = require('express');
-const mongoose = require('mongoose');
+require("dotenv").config(); // read environment variables from .env file
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT;
 
-const expressSwagger = require('express-swagger-generator')(app);
-const options = require('./swagger_conf.js');
+// Allow requests from 'http://localhost:5173'
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+const expressSwagger = require("express-swagger-generator")(app);
+const options = require("./swagger_conf.js");
 expressSwagger(options);
 
 app.use(express.json()); //enable parsing JSON body data
 // root route -- /api/
-app.get('/', function (req, res) {
-    res.status(200).json({
-        message: 'Welcome Indoor Mapping-API'
-    });
+app.get("/", function (req, res) {
+  res.status(200).json({
+    message: "Welcome Indoor Mapping-API",
+  });
 });
 
 // routing middleware
-app.use('/users', require('./routes/users.routes.js'))
-app.use('/beacons', require('./routes/beacons.routes.js'))
-app.use('/userInput', require('./routes/userInput.routes.js'))
+app.use("/users", require("./routes/users.routes.js"));
+app.use("/beacons", require("./routes/beacons.routes.js"));
+app.use("/userInput", require("./routes/userInput.routes.js"));
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@isepbd.szvxel9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@isepbd.szvxel9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.on('open', () => {
-    console.log("connected to MongoDB");
-})
+db.on("error", console.error.bind(console, "connection error:"));
+db.on("open", () => {
+  console.log("connected to MongoDB");
+});
 
 // handle invalid routes
-app.get('*', function (req, res) {
-    res.status(404).json({
-        message: 'WHAT???'
-    });
-})
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.get("*", function (req, res) {
+  res.status(404).json({
+    message: "WHAT???",
+  });
+});
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
