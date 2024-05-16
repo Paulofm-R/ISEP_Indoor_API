cbe773d7422426c6d8fe75111d7cedfb117f79c5
Paulo  Rodrigues <paulofilipe_100pt@hotmail.com>
2024-05-16 12:27:58 +0100

    Update userInput.controller.js

--- controllers/userInput.controller.js
+++ controllers/userInput.controller.js
@@ -3,7 +3,7 @@ const UserInput = require('../models/usersInput.model.js');
 const utilities = require('../utilities/utilities')
 
 exports.create = async (req, res) => {
-    
+
     // Create mew User input
     const userInput = new UserInput({
         userId: req.body.userId,
@@ -11,17 +11,17 @@ exports.create = async (req, res) => {
         satafetionLvl: req.body.satafetionLvl,
         answers: [],
         type: req.body.type,
-        date: new Date (),
+        date: new Date(),
     });
 
     try {
-       // Save user input in database
-       await userInput.save();
-       return res.status(201).json({
-        success: true,
-        msg: "user input was added successfully.",
-        URL: `/userInput/${userInput._id}`
-       });
+        // Save user input in database
+        await userInput.save();
+        return res.status(201).json({
+            success: true,
+            msg: "user input was added successfully.",
+            URL: `/userInput/${userInput._id}`
+        });
 
     } catch (err) {
         if (err.name === "ValidationError") {
@@ -42,11 +42,11 @@ exports.getAll = async (req, res) => {
     try {
         // codigo terem acesso a lista de userInputs
         let data = await UserInput
-        .find()
-        .select('userId Comment satafetionLvl type date resolved')
-        .exec();
+            .find()
+            .select('userId Comment satafetionLvl type date resolved')
+            .exec();
 
-        return res.status(200).json({success: true, userInput: data});
+        return res.status(200).json({ success: true, userInput: data });
 
     } catch (err) {
         if (err.name === "ValidationError") {
@@ -65,9 +65,9 @@ exports.findUserInput = async (req, res) => {
         const userInput = await UserInput.findById(req.params.userInputID).exec();
 
         if (userInput === null)
-            return res.status(404).json({ success: false, msg: `Could not find any user input with the ID ${req.params.userInputID}.`});
+            return res.status(404).json({ success: false, msg: `Could not find any user input with the ID ${req.params.userInputID}.` });
 
-        return res.json({success: true, userInput: userInput});
+        return res.json({ success: true, userInput: userInput });
     } catch (err) {
         if (err.name === "ValidationError") {
             let errors = [];
@@ -82,12 +82,27 @@ exports.findUserInput = async (req, res) => {
 exports.update = async (req, res) => {
     try {
         // codigo para fazer alterações no userInput (como responder, considerar )
-        const userInput = await UserInput.findById(req.params.userInputID, req.body).exec();
+        const userInput = await UserInput.findById(req.params.userInputID).exec();
 
         if (userInput === null)
-            return res.status(404).json({ success: false, msg: `Could not find any user input with the ID ${req.params.userInputID}.`});
+            return res.status(404).json({ success: false, msg: `Could not find any user input with the ID ${req.params.userInputID}.` });
 
-        return res.json({success: true, userInput: userInput});
+        // Atualize os campos do documento com os valores do corpo da requisição
+        Object.keys(req.body).forEach(key => {
+            if (key === 'answers' && Array.isArray(req.body.answers)) {
+                // Adicione as novas respostas ao array existente de "answers"
+                userInput.answers.push(...req.body.answers);
+            } else {
+                // Atualize outros campos
+                userInput[key] = req.body[key];
+            }
+        });
+
+        // Salve o documento atualizado
+        await userInput.save();
+
+
+        return res.json({ success: true, userInput: userInput });
     } catch (err) {
         if (err.name === "ValidationError") {
             let errors = [];
@@ -104,7 +119,7 @@ exports.delete = async (req, res) => {
         // codigo para eliminar um userInputs
         const userInput = await UserInput.findOneAndDelete({ _id: req.params.userInputID }).exec();
 
-        if(!userInput){
+        if (!userInput) {
             return res.status(404).json({ message: `It is not possible to delete the user input with id=${req.params.userInputID}. Perhaps the user input was not found!` });
         } else {
             return res.status(200).json({ message: `User input with id=${req.params.userInputID} was successfully deleted` })
